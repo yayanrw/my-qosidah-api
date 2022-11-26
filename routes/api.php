5c54e1f40bf46898b19c91ba53915ed4a7b52937
yayanrw <yayanraw@gmail.com>
2022-11-26 17:34:27 +0700

    add authcontroller route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/auth/register', 'register');
+    Route::post('/auth/login', 'login');
 });
