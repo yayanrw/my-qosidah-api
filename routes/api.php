aa65d1833b349f172c05af57c58bde24f51f4c9d
yayanrw <yayanraw@gmail.com>
2022-11-26 17:55:20 +0700

    post on register

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::controller(AuthController::class)->group(function () {
-    Route::get('/auth/register', 'register');
+    Route::post('/auth/register', 'register');
     Route::post('/auth/login', 'login');
 });
