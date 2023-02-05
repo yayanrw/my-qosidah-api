87a1a9fc83090d0f3553161ed84a2d28ba9ff872
yayanrw <yayanraw@gmail.com>
2023-02-05 20:08:07 +0700

    add custom http response

---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses
+{
+    protected function success($data, $message = null, $code = 200)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    protected function error($data, $message = null, $code)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+}
