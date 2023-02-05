69be0320dd388e6e8187fe2af6ed9cac2b675d65
yayanrw <yayanraw@gmail.com>
2023-02-06 06:03:09 +0700

    init qosidah detail

---
+++ app/Http/Controllers/QosidahDetailController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\QosidahDetail;
+use App\Http\Requests\StoreQosidahDetailRequest;
+use App\Http\Requests\UpdateQosidahDetailRequest;
+
+class QosidahDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreQosidahDetailRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreQosidahDetailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\QosidahDetail  $qosidahDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(QosidahDetail $qosidahDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\QosidahDetail  $qosidahDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(QosidahDetail $qosidahDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateQosidahDetailRequest  $request
+     * @param  \App\Models\QosidahDetail  $qosidahDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateQosidahDetailRequest $request, QosidahDetail $qosidahDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\QosidahDetail  $qosidahDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(QosidahDetail $qosidahDetail)
+    {
+        //
+    }
+}
