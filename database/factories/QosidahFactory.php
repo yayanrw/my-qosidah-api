3fc554bea23f92636f3ab4124ce4b31221d14d3e
yayanrw <yayanraw@gmail.com>
2023-02-05 21:13:22 +0700

    factory qosidah

--- database/factories/QosidahFactory.php
+++ database/factories/QosidahFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class QosidahFactory extends Factory
@@ -14,7 +15,10 @@ class QosidahFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->name(),
+            'title_latin' => $this->faker->randomElement(['Laa ila ha illallah', 'Subhanallah Walhamdulillah', 'Shollallohu ala muhammad']),
+            'title_translate' => $this->faker->randomElement(['Tiada Tuhan selain Allah', 'Maha Suci Allah', 'Sholawat dan salam kepada Kekasih Nabi Muhammad SAW']),
+            'user_id' => User::all()->random()->id
         ];
     }
 }
