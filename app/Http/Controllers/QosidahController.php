0cb43faa2c98ba12b8ae508d757c1282555be256
yayanrw <yayanraw@gmail.com>
2023-02-05 19:09:03 +0700

    init qosidah

---
+++ app/Http/Controllers/QosidahController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Qosidah;
+use App\Http\Requests\StoreQosidahRequest;
+use App\Http\Requests\UpdateQosidahRequest;
+
+class QosidahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreQosidahRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreQosidahRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Qosidah  $qosidah
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Qosidah $qosidah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Qosidah  $qosidah
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Qosidah $qosidah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateQosidahRequest  $request
+     * @param  \App\Models\Qosidah  $qosidah
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateQosidahRequest $request, Qosidah $qosidah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Qosidah  $qosidah
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Qosidah $qosidah)
+    {
+        //
+    }
+}
